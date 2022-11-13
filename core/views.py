b82b30c287046417405335833ea88f7a07454916
joaojv90 <joao.jaramillo@gmail.com>
2022-11-12 21:46:36 -0500

    creation and merging of views

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def portfolio(request):
+    return render(request, 'core/portfolio.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
